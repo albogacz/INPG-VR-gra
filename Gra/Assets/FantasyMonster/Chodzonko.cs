914a06a113eda1e47a305364224b1cd6e0948cbc
WykwalifikowanyProgramista2000 <wdziuba@student.agh.edu.pl>
2018-06-03 21:33:13 +0200

    Dodano szkieleciki z AI

---
+++ Gra/Assets/FantasyMonster/Chodzonko.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Chodzonko : MonoBehaviour
+{
+
+    public Transform player;
+    static Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Vector3 direction = player.position - this.transform.position;
+        float angle = Vector3.Angle(direction, this.transform.forward);
+        if (Vector3.Distance(player.position, this.transform.position) < 10 && angle <30)
+        {
+            direction.y = 0;
+
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
+
+            anim.SetBool("isIdle", false);
+            if (direction.magnitude > 2)
+            {
+                this.transform.Translate(0,0,0.05f);
+                anim.SetBool("isWalking", true);
+                anim.SetBool("isAttacking",false);
+            }
+            else
+            {
+                anim.SetBool("isAttacking", true);
+                anim.SetBool("isWalking", false);
+            }
+        }
+        else
+        {
+            anim.SetBool("isIdle", true);
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isAttacking", false);
+        }
+    }
+}
---
+++ Gra/Assets/GameStart.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStart : MonoBehaviour
+{
+    private void Awake()
+    {
+        StartCoroutine(WaitForIt(3.0F));
+    }
+
+    
+       //SceneManager.LoadScene(nextLevel);
+        IEnumerator WaitForIt(float waitTime)
+         {
+             yield return new WaitForSeconds(waitTime);
+             UnityEngine.SceneManagement.SceneManager.LoadScene("main_map");
+         }
+
+
+}
+
