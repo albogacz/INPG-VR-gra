3249d73b0e47dfee446de80fb0af2d2ad3e7492d
albogacz <anna.bogacz298@gmail.com>
2018-06-09 14:37:51 +0200

    daiy + skrypt

---
+++ Gra/Paczki/FallUpdate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallUpdate : MonoBehaviour {
+
+	
+	void Start () {
+
+        FixedUpdate();
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (transform.position.y < -15)
+        {
+            MoveBack();
+        }
+    }
+
+    void MoveBack()
+    {
+
+        transform.position = new Vector3(-2, 3, 33);
+
+    }
+}
